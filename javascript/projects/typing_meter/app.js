5cbd467a21df7ae912b275206e47d206d45ecde0
pdorji1 <pdorji@infraclear.com>
2022-02-06 20:07:37 -0500

    accuracy feature

--- javascript/projects/typing_meter/app.js
+++ javascript/projects/typing_meter/app.js
@@ -1,6 +1,7 @@
 let startText = "Press Start To Begin!"
 let text = "A short story is a piece of prose fiction that typically can be read in one sitting and focuses on a self-contained incident or series of linked incidents, with the intent of evoking a single effect or mood. The short story is one of the oldest types of literature and has existed in the form of legends, mythic tales, folk tales, fairy tales, tall tales, fables and anecdotes in various ancient communities around the world. The modern short story developed in the early 19th century."
 let slider = 0;
+let char_count = 0;
 let STARTED = false;
 let start_time = 0;
 const MAX_WPM = 100;
@@ -8,6 +9,8 @@ const text_color_typed = "#FFFFFF";
 const text_color_untyped = "#501717";
 const text_color_next = "orange";
 const start_speed = "0 wpm";
+const start_accuracy = "0 %";
+const start_timer = "00 s";
 const individual_char = "individual-char";
 
 // text = "test"
@@ -24,16 +27,29 @@ button.addEventListener("click",startTyper);
 let body = document.body
 body.addEventListener("keypress",keyPress)
 
-setInterval(showSpeed,100)// every 10 secondsw
+setInterval(showStats,100)// every 10 secondsw
 
-function showSpeed(){
+function showStats(){
     if (STARTED===true){
+        // speed update
         let now = Date.now();
         let time_elapsed = (now - start_time)/60000 // minutes
-        let disp = document.getElementById("speed-value");
         let words = (1/4.7) * slider // average number of chars in a word
         let wpm = Math.round(words/time_elapsed);
-        disp.textContent = wpm + " " + start_speed.split(" ")[1]
+        let speed = wpm + " " + start_speed.split(" ")[1]
+
+
+        //accuracy update
+        let acc = Math.round((slider/char_count) * 100) 
+        if (acc===NaN){
+            acc = 0;
+        }
+        let accuracy = acc + " " + start_accuracy.split(" ")[1]
+
+        //timer update
+        let timer = start_timer;
+
+        update_stats(speed,accuracy,timer)
     }
 }
 
@@ -43,16 +59,16 @@ function keyPress(event){
         let letters = document.getElementsByClassName(individual_char)
         let curr = letters[slider].textContent
         let typed = String.fromCharCode(event.keyCode)
+        char_count +=1;
         if (curr===typed){
             //change color of typed char
             letters[slider].style.color = text_color_typed; 
             slider += 1;
             if (slider === text.length){
-                slider = 0;
-                startTyper(NaN);
+                reset()
                 return
             }
-            //change color of 
+            //change color of next char to type
             letters[slider].style.color = text_color_next;
         }        
     }
@@ -82,11 +98,26 @@ function startTyper(event){
         start_time = Date.now();
     
     } else{
-        let disp = document.getElementById("speed-value");
-        disp.textContent = start_speed;
-        p_elem.textContent = startText
-        STARTED = false;
-        start_time = 0;
+       reset()
     }
 
+}
+
+function reset(){
+    let p_elem = document.getElementById("text-box")
+    p_elem.textContent = startText;
+    update_stats(start_speed,start_accuracy,start_timer)
+
+    STARTED = false;
+    start_time = 0;
+    char_count = 0;
+}
+
+function update_stats(speed,accuracy,timer){
+    let s = document.getElementById("speed-value");
+    let a = document.getElementById("accuracy-value");
+    let t = document.getElementById("timer-value");
+    s.textContent = speed;
+    a.textContent = accuracy;
+    t.textContent = timer;    
 }
